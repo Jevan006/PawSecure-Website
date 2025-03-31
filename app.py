d3c3cb4666cb97cb2a83eb59c01ab88bca9d9cf5
Jevan006 <jevan.peters@sanlam.co.za>
2025-03-31 06:50:21 +0200

    app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+from configurations import Config
+from extensions import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
