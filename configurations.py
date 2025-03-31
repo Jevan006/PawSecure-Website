cb5df448156c79a63e3f1f141732aa1ab0d2b87b
Jevan006 <jevan.peters@sanlam.co.za>
2025-03-31 07:32:08 +0200

    installations

--- configurations.py
+++ configurations.py
@@ -2,7 +2,5 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pymssql://MUD/E1009817:@csEHPAf*@Gv3rj@PF2WGETC/SQLEXPRESS/database_name"
-    )
+    SQLALCHEMY_DATABASE_URI = "mssql+pymssql://MUD/E1009817:@csEHPAf*@Gv3rj@PF2WGETC/SQLEXPRESS/PawSecureDB?driver=ODBC+Driver+17+for+SQL+Server"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
